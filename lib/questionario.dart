fa9bb0affac6595c9690a9784d3c259f1ccf2b41
Lucas <web.m.lucasalves@outlook.com>
2021-12-02 00:45:33 -0300

    first commit

---
+++ lib/questionario.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import './questao.dart';
+import './resposta.dart';
+
+class Questionario extends StatelessWidget {
+  final List<Map<String, Object>> perguntas;
+  final int perguntaSelecionada;
+  final void Function(int) quantoResponder;
+
+  Questionario({
+    required this.perguntas,
+    required this.perguntaSelecionada,
+    required this.quantoResponder,
+  });
+
+  bool get temPerguntaSelecionada {
+    return perguntaSelecionada < perguntas.length;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, Object>> respostas = temPerguntaSelecionada
+        ? perguntas[perguntaSelecionada].cast()['respostas']
+        : [];
+    return Column(
+      children: <Widget>[
+        Questao(perguntas[perguntaSelecionada]['texto'].toString()),
+        ...respostas.map((resp) => Resposta(resp['texto'].toString(),
+            () => quantoResponder(int.parse(resp['pontuacao'].toString()))))
+      ],
+    );
+  }
+}
